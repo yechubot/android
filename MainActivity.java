6ddf3e0a01291a9ee02e6732489764ac2c542284
yechubot <61838900+yechubot@users.noreply.github.com>
2020-10-14 11:13:00 +0900

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,77 +1,75 @@
-package com.example.widgettest;
+package com.example.tablecalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.RadioButton;
+import android.widget.TableRow;
+import android.widget.TextView;
 import android.widget.Toast;
 
-
-public class MainActivity extends AppCompatActivity { // activity 에 관련된 기능 상속받음
-    EditText edtInput; // 선언만 함
-    Button btnTextShow, btnHomeOpen;
-    RadioButton rdoDog, rdoCat;
-    ImageView ivPet;
-    String url; // 밖에 선언-> 전역변수 -> 모든 메서드에서 호출 가능
-
+public class MainActivity extends AppCompatActivity {
+    EditText etInput1, etInput2;
+    Button[] numButtons = new Button[10];
+    int numButtonsIDs[]={R.id.Num0, R.id.Num1, R.id.Num2, R.id.Num3, R.id.Num4,
+            R.id.Num5, R.id.Num6, R.id.Num7, R.id.Num8, R.id.Num9};
+    Button btnAdd, btnSub, btnMul, btnDiv;
+    TextView textResult;
+    String stringNum1, stringNum2;
+    int result, i;
     @Override
-    protected void onCreate(Bundle savedInstanceState) { // activity 생성 메서드
-        super.onCreate(savedInstanceState); // 부모onCreate 기능 받음
-        setContentView(R.layout.activity_main); // activity 화면 setting , view => 위젯 총칭
-        // 위젯 위치가 Resources-> layout -> activiy_main 이라는 파일에 있다
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        // 위젯과 연결
-        //(EditText)안써도 됨 but 쓰면 명시
-        edtInput = (EditText)findViewById(R.id.edtInput);
-        btnTextShow = (Button)findViewById(R.id.btnTextShow);
-        btnHomeOpen = (Button)findViewById(R.id.btnHomeOpen);
-        rdoDog = (RadioButton)findViewById(R.id.rdoDog);
-        rdoCat = (RadioButton)findViewById(R.id.rdoCat);
-        ivPet = (ImageView)findViewById(R.id.ivPet);
+        etInput1=(EditText)findViewById(R.id.etInput1);
+        etInput2=(EditText)findViewById(R.id.etInput2);
+        for(int i =0; i<numButtons.length; i++){
+            numButtons[i]=(Button)findViewById(numButtonsIDs[i]);
+        }
+        btnAdd=(Button)findViewById(R.id.btnAdd);
+        btnSub=(Button)findViewById(R.id.btnSub);
+        btnMul=(Button)findViewById(R.id.btnMul);
+        btnDiv=(Button)findViewById(R.id.btnDiv);
+        textResult=(TextView)findViewById(R.id.textResult);
 
-        //editText 글자 입력 후, 버튼 클릭하면 토스트로 보이기
-        btnTextShow.setOnClickListener(new View.OnClickListener() { // Interface OnClickListener 익명 클래스에서 구현
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),edtInput.getText().toString(),Toast.LENGTH_SHORT).show();// toast에 보일 글자를 만드는 메서드
-                //숫자 넣으면 앱 종료됨 string이 아니라서
-            }
-        });
-        // editText에 홈페이지 주소를 입력 후 , 버튼 클릭하면 브라우저 통해 홈페이지 보이기
-        btnHomeOpen.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View v) {
-                //http 입력 없이도 url 열리게 만들기
-                url = edtInput.getText().toString();
-                if(!url.substring(0,7).equals("http://")){
-                    url= "http://"+url;
+        for(int i=0; i<numButtons.length; i++){
+            final int index;
+            index=i;
+            numButtons[index].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(etInput1.isFocused()==true){
+                        stringNum1 =etInput1.getText().toString()+numButtons[index].getText().toString();
+                        //etInput1.getText().toString()으로 직접 입력한것도 함께 작성되도록함
+                        etInput1.setText(stringNum1);
+                    }else if(etInput2.isFocused()==true){
+                        stringNum2 =etInput2.getText().toString()+numButtons[index].getText().toString();
+                        etInput2.setText(stringNum2);
+                    }else {
+                        Toast.makeText(getApplicationContext(),"입력창을 선택한 다음 숫자를 입력하세요", Toast.LENGTH_SHORT).show();
+                    }
                 }
-                Intent mintent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-                startActivity(mintent);
+            });
+        }
 
-            }
-        });
-        // 강아지/고양이 라디오 버튼을 클릭하면 imageView에 강아지/고양이 보이기
-        rdoDog.setOnClickListener(new View.OnClickListener() {
+        btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ivPet.setImageResource(R.drawable.dog);
+                stringNum1 = etInput1.getText().toString();
+                stringNum2 = etInput2.getText().toString();
+                result=Integer.parseInt(stringNum1)+Integer.parseInt(stringNum1);
+
+                textResult.setText("계산 결과: "+result);
             }
         });
 
-        rdoCat.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ivPet.setImageResource(R.drawable.cat);
-            }
-        });
+
+
+
+
+
     }
 }
