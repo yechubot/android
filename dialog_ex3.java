b658c2a7ef926d8fa980f35bb590d1e292d21afd
yechubot <61838900+yechubot@users.noreply.github.com>
2020-10-22 12:33:56 +0900

    Update dialog_ex3.java

--- dialog_ex3.java
+++ dialog_ex3.java
@@ -10,23 +10,51 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
     Button btnInput;
-    TextView tvName, tvEmail;
-    EditText dialog_edtName, dialog_edtEmail;
+    /*    TextView tvName, tvEmail;
+        EditText dialog_edtName, dialog_edtEmail;*/
     View dialogView; // dialog 전용 뷰
+    EditText edtName, edtEmail;
+    TextView dialog_name, dialog_email;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnInput = findViewById(R.id.btnInput);
+        edtName = findViewById(R.id.edtName);
+        edtEmail = findViewById(R.id.edtEmail);
+
+        btnInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialogView = View.inflate(MainActivity.this, R.layout.dialog2, null);
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("입력된 사용자 정보");
+                builder.setIcon(R.drawable.friends);
+                dialog_email = dialogView.findViewById(R.id.dialog_email);
+                dialog_name = dialogView.findViewById(R.id.dialog_name);
+                builder.setView(dialogView);
+                dialog_name.setText(edtName.getText().toString());
+                dialog_email.setText(edtEmail.getText().toString());
+                builder.setPositiveButton("확인",null);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+
+
+/* // dialog1
         tvName = findViewById(R.id.tvName);
         tvEmail = findViewById(R.id.tvEmail);
 
         btnInput.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                dialogView = View.inflate(MainActivity.this,R.layout.dialog1,null);  // dialog xml을 변수에 담음
+                dialogView = View.inflate(MainActivity.this, R.layout.dialog1, null);  // dialog xml을 변수에 담음
                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
                 builder.setTitle("사용자 정보 입력");
                 builder.setIcon(R.drawable.friends);
@@ -45,6 +73,6 @@ public class MainActivity extends AppCompatActivity {
                 dialog.show();
 
             }
-        });
+        });*/
     }
 }
