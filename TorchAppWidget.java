308bfe2b33d5ce187ac39b5fb561acaf39890ad0
yeeun <61838900+yechubot@users.noreply.github.com>
2020-12-14 12:51:10 +0900

    Add files via upload

---
+++ TorchAppWidget.java
@@ -0,0 +1,46 @@
+package com.example.appwidget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+
+public class TorchAppWidget extends AppWidgetProvider {//appwidgetprovider -> broadcast receiver 의 일종
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.torch_app_widget);
+        views.setTextViewText(R.id.appwidget_text, widgetText);
+        //추가
+        Intent intent = new Intent(context,TorchService.class);
+        PendingIntent pendingIntent = PendingIntent.getService(context,0,intent,0);
+        views.setOnClickPendingIntent(R.id.appwidget_layout, pendingIntent); //정의 끝
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
--- TorchService.java
+++ TorchService.java
@@ -9,6 +9,7 @@ import androidx.annotation.Nullable;
 
 public class TorchService extends Service {
     Torch torch;
+    Boolean isRunning = false;
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -28,19 +29,35 @@ public class TorchService extends Service {
     //서비스 수행시 제일 먼저 수행되는 메소드
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        if(intent.getAction().equals("on")){
+        if(intent.getAction()=="on"){
             try {
                 torch.flashOn();
+                isRunning = true;
             } catch (CameraAccessException e) {
                 e.printStackTrace();
             }
-        }else if(intent.getAction().equals("off")){
+        }else if(intent.getAction()=="off"){
             try {
                 torch.flashOff();
+                isRunning = false;
             } catch (CameraAccessException e) {
                 e.printStackTrace();
             }
         }else {
+            isRunning =!isRunning; //값 반전시킴
+            if(isRunning){
+                try {
+                    torch.flashOn();
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+            }else {
+                try {
+                    torch.flashOff();
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+            }
 
         }
         return super.onStartCommand(intent, flags, startId);
